4c194de2dbb9c013c271fbf740f34deae1c8496b
Shrihari T <shriharit2004@gmail.com>
2024-11-21 19:07:27 +0530

    sync commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const wbm = require('wbm');
-
-wbm.start().then(async () => {
-    const phones = ['919789114693'];
-    const message = 'Good Morning.';
-    await wbm.send(phones, message);
-    await wbm.end();
-}).catch(err => console.log(err));
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-bank_statement_location = os.getenv("BANK_STATEMENT_LOCATION")
