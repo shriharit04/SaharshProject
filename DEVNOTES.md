43f6653626a4b81821a01a6be60d59ee537a1a3b
Shrihari T <shriharit2004@gmail.com>
2024-12-20 13:00:20 +0530

    retrieve from excel in required format. mention due months also.

--- DEVNOTES.md
+++ DEVNOTES.md
@@ -1,5 +1,6 @@
 # Checklist
 
-- change template
-- set datainputs
-
+- need to understand {{}} and {} in html template code
+- 15th reminder
+- 30th reminder
+- cumulative reminder
--- autoMail.py
+++ autoMail.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from mailjet_rest import Client
+import pandas as pd
 import os
+import datetime 
+
 load_dotenv()
 
 api_key = os.environ['MJ_APIKEY_PUBLIC']
@@ -8,71 +11,118 @@ api_secret = os.environ['MJ_APIKEY_PRIVATE']
 mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 
 
-
-def Send_mail(recv_data):
+def send_mail(recv_data):
     sender_mail = "updates@saharshapts.co.in"
-    recv_data["mail_id"] = "ntshrihari@gmail.com"
+    curr_month  = datetime.datetime.now().strftime('%B')
+    months = recv_data.get("months",[])
+    if(recv_data['outstanding_bal']==0):
+        months = "NA"
+    else:
+        months = ", ".join(months)
+
     
-    ''' template contains : 
-        {name}} , {{flat_no}} , {{outstanding_bal}}: Outstanding balance
-        {{month}} The month for which the maintenance payment is due , {{year}} '''
     
     # define class for outstanding_bal in html 
     if recv_data['outstanding_bal'] > 0:
         outstanding_bal_class = "due"
-        status = "Pending Due"
+        status = "Pending"
     else:
         outstanding_bal_class = "paid"
         status = "Paid"
 
 
     # read html
-    with open("test_template.html", "r") as file:
+    with open("due_update_template.html", "r") as file:
         html_content = file.read().strip()
         html_content = html_content.format(
             name=recv_data["name"],
             flat_no=recv_data["flat_no"],
             outstanding_bal=recv_data["outstanding_bal"],
-            month=recv_data["month"],
+            month= curr_month,
             outstanding_bal_class=outstanding_bal_class,
-            status=status
+            status=status,
+            months = months
         )
 
     data = {
         'Messages': [
                         {
-                            "From": {
-                                    "Email": sender_mail ,
-                                    "Name": "Saharsh Association"
-                            },
+                            "From": { "Email": sender_mail , "Name": "Saharsh Association"},
                             "To": [
-                                    {
-                                            "Email": recv_data["mail_id"],
-                                            "Name": recv_data["name"]
-                                    }
+                                    { "Email": recv_data["email_id"], "Name": recv_data["name"]}
                             ],
-                            "Subject": f"Outstanding Balance for {recv_data['month']}",
-                            "TextPart": f"Outstanding Balance for {recv_data['month']}",
+                            "Subject": f"Outstanding Balance for {curr_month}",
+                            "TextPart": f"Outstanding Balance for {curr_month}",
                             "HTMLPart": html_content
                         }
                 ]
         }
     result = mailjet.send.create(data=data)
-    # print(result.json())
-    res_data = result.json()        
+    response = result.json()
+    status = response['Messages'][0]['Status']
+    to_email = response['Messages'][0]['To'][0]['Email']  
+    if status == 'success':
+        # print("Email sent successfully!")
+        pass
+    else:
+        print(f"Failed to send email to {to_email}.")
+
+def process_data(file_path):
+    df = pd.read_excel(file_path)
+
+    month_order = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    df.columns = df.columns.str.strip()
+    df['month'] = pd.Categorical(df['month'], categories=month_order, ordered=True)
+    df = df.sort_values(by=['flat_no', 'month'])
+    flat_array = []  
+    last_flat_no = None 
+    temp_flat = {} 
+
+    for index, row in df.iterrows():
+        if last_flat_no != row['flat_no']:
+
+            if last_flat_no is not None:
+                flat_array.append(temp_flat)
+            
+            # Reset the temp dictionary when flat_no changes
+            temp_flat = {}
+            temp_flat = {
+                "name": row['name'],
+                "email_id": row['email_id'],
+                "flat_no": row['flat_no'],
+                "outstanding_bal" : 0,
+                "months": [],  
+                "due_dates": [],  
+                "due_amounts": []  
+
+            }
+        
+        # Append
+        temp_flat["months"].append(row['month'])
+        temp_flat["due_dates"].append(row['due_date'])
+        temp_flat["due_amounts"].append(row['due_amt'])
+        temp_flat["outstanding_bal"] += row['due_amt'] 
+        
+        last_flat_no = row['flat_no']
+
+    if temp_flat:
+        flat_array.append(temp_flat)
+
+    return flat_array
 
 
 
 
-test_data = [ {"name": "John Doe", "flat_no": "A-305", "outstanding_bal": 500, "month": "December"}, 
-             {"name": "Jane Smith", "flat_no": "B-102", "outstanding_bal": 0, "month": "December"}, 
-             {"name": "Raj Kumar", "flat_no": "C-204", "outstanding_bal": -1500, "month": "November"}, 
-             {"name": "Priya Sharma", "flat_no": "D-401", "outstanding_bal": 250, "month": "October"}, 
-             {"name": "Anil Gupta", "flat_no": "E-507", "outstanding_bal": 800, "month": "December"} ]
 
-for record in test_data:
-    print(record)
-    Send_mail(record)
+
+
+
+excel_file = "Book1.xlsx"
+flats_data = process_data(excel_file)
+for flat in flats_data[:5]:
+    send_mail(flat)
+
 
 
 
